c7ab637fca82b1a5ffd319ed373949cbaf5ca4e1
Alexander <kkruscherr@gmail.com>
2024-06-15 20:04:31 +0300

    Added Anonymous User Route Tests

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -12,21 +12,23 @@ class TestRoutes(TestCase):
     def setUpTestData(cls) -> None:
         cls.news = News.objects.create(title='Заголовок', text='Текст')
 
-    def test_home_page(self):
-        url = reverse('news:home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_detail_page(self):
-        url = reverse('news:detail', args=(self.news.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
 # =============================================================================
-# Страница отдельной новости доступна анонимному пользователю.
 # Страницы удаления и редактирования комментария доступны автору комментария.
 # При попытке перейти на страницу редактирования или удаления комментария анонимный пользователь перенаправляется на страницу авторизации.
 # Авторизованный пользователь не может зайти на страницы редактирования или удаления чужих комментариев (возвращается ошибка 404).
-# Страницы регистрации пользователей, входа в учётную запись и выхода из неё доступны анонимным пользователям.
 # =============================================================================
