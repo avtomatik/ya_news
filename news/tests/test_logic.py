29c21ccab415675201bd5658f12fffa4db1eff48
Alexander <kkruscherr@gmail.com>
2024-06-16 01:22:32 +0300

    Added Tests For Editing & Deleting the Comments

--- news/tests/test_logic.py
+++ news/tests/test_logic.py
@@ -1,7 +1,4 @@
-# =============================================================================
-# Авторизованный пользователь может редактировать или удалять свои комментарии.
-# Авторизованный пользователь не может редактировать или удалять чужие комментарии.
-# =============================================================================
+from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
@@ -52,3 +49,53 @@ class TestCommentCreation(TestCase):
         )
         comments_count = Comment.objects.count()
         self.assertEqual(comments_count, 0)
+
+
+class TestCommentEditDelete(TestCase):
+
+    COMMENT_TEXT = 'Текст комментария'
+    NEW_COMMENT_TEXT = 'Обновлённый комментарий'
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+        news_url = reverse('news:detail', args=(cls.news.id,))
+        cls.url_to_comments = news_url + '#comments'
+        cls.author = User.objects.create(username='Автор комментария')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.reader = User.objects.create(username='Читатель')
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text=cls.COMMENT_TEXT
+        )
+        cls.edit_url = reverse('news:edit', args=(cls.comment.id,))
+        cls.delete_url = reverse('news:delete', args=(cls.comment.id,))
+        cls.form_data = {'text': cls.NEW_COMMENT_TEXT}
+
+    def test_author_can_delete_comment(self):
+        response = self.author_client.delete(self.delete_url)
+        self.assertRedirects(response, self.url_to_comments)
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 0)
+
+    def test_user_cant_delete_comment_of_another_user(self):
+        response = self.reader_client.delete(self.delete_url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 1)
+
+    def test_author_can_edit_comment(self):
+        response = self.author_client.post(self.edit_url, data=self.form_data)
+        self.assertRedirects(response, self.url_to_comments)
+        self.comment.refresh_from_db()
+        self.assertEqual(self.comment.text, self.NEW_COMMENT_TEXT)
+
+    def test_user_cant_edit_comment_of_another_user(self):
+        response = self.reader_client.post(self.edit_url, data=self.form_data)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.comment.refresh_from_db()
+        self.assertEqual(self.comment.text, self.COMMENT_TEXT)
