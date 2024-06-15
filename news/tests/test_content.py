61191c6e23a3205e4039229fd813762d75d7a681
Alexander <kkruscherr@gmail.com>
2024-06-15 21:59:45 +0300

    Added Test For News Count on Home Page LTE 10

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,6 +1,5 @@
 
 # =============================================================================
-# Количество новостей на главной странице — не более 10.
 # Новости отсортированы от самой свежей к самой старой. Свежие новости в начале списка.
 # Комментарии на странице отдельной новости отсортированы от старых к новым: старые в начале списка, новые — в конце.
 # Анонимному пользователю недоступна форма для отправки комментария на странице отдельной новости, а авторизованному доступна.
@@ -8,11 +7,14 @@
 
 from django.conf import settings
 from django.test import TestCase
+from django.urls import reverse
 
 from news.models import News
 
 
-class TestContent(TestCase):
+class TestHomePage(TestCase):
+
+    HOME_URL = reverse('news:home')
 
     @classmethod
     def setUpTestData(cls) -> None:
@@ -20,3 +22,9 @@ class TestContent(TestCase):
             News(title=f'Новость {index}', text='Просто текст.')
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         )
+
+    def test_news_count(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        news_count = object_list.count()
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
