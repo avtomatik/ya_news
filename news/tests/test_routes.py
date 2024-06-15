53f75bdcfa5b4fec34f1e657fe7bdbd54e0fc93b
Alexander <kkruscherr@gmail.com>
2024-06-15 15:10:07 +0300

    Improved Home Page Test

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,11 +1,15 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
 
 
 class TestRoutes(TestCase):
 
     def test_home_page(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        url = reverse('news:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
 # =============================================================================
