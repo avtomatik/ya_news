4c26c97c6f664a0feec14ab61c190be83ef28c2a
Alexander <kkruscherr@gmail.com>
2024-06-15 19:50:44 +0300

    Added Naïve News Detail Route Test

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -3,11 +3,22 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from news.models import News
+
 
 class TestRoutes(TestCase):
 
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+
     def test_home_page(self):
         url = reverse('news:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_detail_page(self):
+        url = reverse('news:detail', args=(self.news.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
