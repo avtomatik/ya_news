ed49c7a12bae6e94c07cf36ea2b36a6cef162ec5
Alexander <kkruscherr@gmail.com>
2024-06-16 00:30:36 +0300

    Added Test For Anonymous User Can't Create Comment

--- news/tests/test_logic.py
+++ news/tests/test_logic.py
@@ -1,1 +1,33 @@
-# =============================================================================# Анонимный пользователь не может отправить комментарий.# Авторизованный пользователь может отправить комментарий.# Если комментарий содержит запрещённые слова, он не будет опубликован, а форма вернёт ошибку.# Авторизованный пользователь может редактировать или удалять свои комментарии.# Авторизованный пользователь не может редактировать или удалять чужие комментарии.# =============================================================================
+# =============================================================================
+# Авторизованный пользователь может отправить комментарий.
+# Если комментарий содержит запрещённые слова, он не будет опубликован, а форма вернёт ошибку.
+# Авторизованный пользователь может редактировать или удалять свои комментарии.
+# Авторизованный пользователь не может редактировать или удалять чужие комментарии.
+# =============================================================================
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from news.models import Comment, News
+
+User = get_user_model()
+
+
+class TestCommentCreation(TestCase):
+
+    COMMENT_TEXT = 'Текст комментария'
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+        cls.url = reverse('news:detail', args=(cls.news.id,))
+        cls.user = User.objects.create(username='Мимо Крокодил')
+        cls.auth_client = Client()
+        cls.auth_client.force_login(cls.user)
+        cls.form_data = {'text': cls.COMMENT_TEXT}
+
+    def test_anonymous_user_cant_create_comment(self):
+        self.client.post(self.url, data=self.form_data)
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 0)
